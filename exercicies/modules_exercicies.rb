54db1d152eeacc7b3dedc4ec75f75609e7183428
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-05-18 07:56:33 -0300

    More example with module

--- exercicies/modules_exercicies.rb
+++ exercicies/modules_exercicies.rb
@@ -35,4 +35,37 @@ module Person
 end 
 
 empresa = Person::Juridic.new("L7 Logistica", "641.5150.2051/000-1").add 
-pessoaf = Person::Physical.new("Thiago Bolsobosta", "0212.515.0551.12").add 
+pessoaf = Person::Physical.new("Thiago Bolsobosta", "0212.515.0551.12").add
+
+# Include module in class for instances class use
+module NameMethods
+  def upperName
+    self.name.upcase
+  end
+end
+
+class User
+  attr_accessor :name
+  include NameMethods
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+puts User.new("maria").upperName
+
+# In this example the class extends the module and use it
+module NameMethods
+  def nameClass
+    "#{self} é uma classe que estende o módulo NameMethods"
+  end
+end
+
+class Admin
+  extend NameMethods
+end
+
+
+puts Admin.nameClass
