1d1c08a4299e9146b2afac6999b1501ea77118ea
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2021-07-17 19:17:59 -0300

    more exercises for group_by

--- exercises/groupy_by.rb
+++ exercises/groupy_by.rb
@@ -6,3 +6,20 @@ end
 
 marks = {"Ramesh":23, "Vivek":40, "Harsh":88, "Mohammad":60}
 group_by_marks(marks, 30)
+
+#------------------------------------------------------------------------------------------------
+
+# Grupo de números divididos por 3 e os que não são
+(1..20).group_by {|n| n % 3 == 0}
+
+#------------------------------------------------------------------------------------------------
+
+# Agrupar bandas em ordem alfabetica
+bandas = %w(Metallica Megadeath JudasPriest Beatles Yes PinkFloyd BlackSabbath Aerosmith)
+bandas.sort!
+
+bandas.group_by {|b| b[0].to_sym} # Aqui eu agrupo por letra inicial
+# E aqui eu agrupo também por letra, mas informo o total de bandas que começam por aquela inicial
+bandas.group_by {|b| b[0].to_sym}.transform_values {|value| value.count}
+
+#------------------------------------------------------------------------------------------------
